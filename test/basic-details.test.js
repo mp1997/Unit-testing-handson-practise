85eded02c2c83aeee55b198abc24e0e414a3fdfb
mp1997 <40606403+mp1997@users.noreply.github.com>
2022-06-23 10:53:59 +0000

    Update basic-details.test.js

--- test/basic-details.test.js
+++ test/basic-details.test.js
@@ -1,8 +1,17 @@
 import { html, fixture, expect } from '@open-wc/testing';
 import { stub } from 'sinon';
 import '../src/LoanBasicDetails/BasicDetails.js';
+import { format } from 'prettier';
 
 describe('Basic details', () => {
   // Write test cases inside this block
   // refer basic-details.js files
+
+  it('check for spy func',()=>{
+    const el=fixture(html`<basic-details></basic-details>`);
+    const spy=Sinon.spy(el,'_toDashboard');
+    form.click();
+    form.click();
+    expect(spy.calledTwice).to.be.true;
+  });
 });
