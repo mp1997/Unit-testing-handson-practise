31b768d8628c6af67c762d026ec3e7461966b2ac
mp1997 <40606403+mp1997@users.noreply.github.com>
2022-06-23 10:49:57 +0000

    Update loan-application.test.js

--- test/loan-application.test.js
+++ test/loan-application.test.js
@@ -1,7 +1,16 @@
 import { html, fixture, expect } from '@open-wc/testing';
+import Sinon from 'sinon';
+import {stub} from 'sinon';
+import * as LoanApplication from '../src/LoanApplication.js';
 
 import '../loan-application.js';
 
 describe('LoanApplication', () => {
   // Write test cases inside this block
+
+  it('test the increment',async() => {
+    const el = await fixture(html`LoanApplication></LoanApplication>`);
+    const spy = Sinon.spy(el, '_increment').returns(true);
+    expect(spy.calledOnce).to.be.true;
+  });
 });
