034ca066bd0873f522315fdefb92aa477bc1e70e
M Priyanka <40606403+mp1997@users.noreply.github.com>
2022-06-23 18:03:22 +0000

    Update customer-details.test.js

--- test/customer-details.test.js
+++ test/customer-details.test.js
@@ -4,4 +4,15 @@ import '../src/Customer/Customer-details.js';
 
 describe('customer details', () => {
   // Write test cases inside this block
+
+  it('check for events',async()=>{
+    const el=await fixture(html`<customer-details></customer-details>`);
+    await el.shadowRoot.querySelector('#nextbtn').click();
+  });
+
+  it('contain valid characters in FirstName',async()=>{
+    const el=await fixture(html`<lion-input label="${localize.msg('change-language:firstname')}"></lion-input>`);
+    await expect(el.label).to.match(/^[a-zA-Z\s]*$/);
+  });
+
 });
