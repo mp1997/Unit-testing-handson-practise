85d160409c2568277685a230d79b8eba47406aa7
mp1997 <40606403+mp1997@users.noreply.github.com>
2022-06-23 10:53:08 +0000

    Update errorAndSuccess.test.js

--- test/errorAndSuccess.test.js
+++ test/errorAndSuccess.test.js
@@ -5,8 +5,18 @@ import '../src/SuccessAndError/Error.js';
 
 describe('Success screen ', () => {
   // Write test cases inside this block
+
+  const el=await fixture(html`<loan-success></loan-success>`);
+  const spy=Sinon.spy(el,'_toHome');
+  spy();
+  expect(spy.called).to.be.true;
 });
 
 describe('error screen', () => {
   // Write test cases inside this block
+
+  const el=await fixture(html`<loan-error></loan-error>`);
+  const spys=Sinon.spy(el,'_toHome');
+  spys();
+  expect(spys.called).to.be.true;
 });
