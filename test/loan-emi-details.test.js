5ff9370cddee5481b93d3fee8b0b3a528b77e57f
mp1997 <40606403+mp1997@users.noreply.github.com>
2022-06-23 10:52:23 +0000

    Update loan-emi-details.test.js

--- test/loan-emi-details.test.js
+++ test/loan-emi-details.test.js
@@ -1,6 +1,15 @@
 import { html, fixture, expect } from '@open-wc/testing';
+import {stub} from
 import '../src/LoanEMIDetails/LoanEMIDetails.js';
 
 describe('Loan EMI details', () => {
   // Write test cases inside this block
+
+  it('check for spy func',async ()=>{
+    const el=await fixture(html`<loanemi-details></loanemi-details>`);
+    const spy=Sinon.spy(el,'_toCustomer');
+    form.click();
+    form.click();
+    expect(spy.calledTwice).to.be.true;
+  });
 });
